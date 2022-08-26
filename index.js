98839f375dec93654236032fd827c1723694b52d
Travis <travisliu708@gmail.com>
2022-08-25 18:26:13 -0700

    added base and recursive case to solve problem

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  //base case when fromN reaches toN
+  if (fromN === toN) {
+    return toN
+  }
+
+  return fromN + sum(fromN +1, toN);
+};
+
+
+
+console.log(sum(3, 7));
 
 module.exports = sum;
